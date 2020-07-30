a3f6adfeff0feeb6da18aee105abfd821f6e890c
Christian <christian@chfi.se>
2020-07-30 17:51:48 -0500

    use GAFPath in GAF type, GAF line parser; add GAF parser tests

--- src/gafpaf.rs
+++ src/gafpaf.rs
@@ -1,28 +1,18 @@
 use bstr::{BStr, BString, ByteSlice, ByteVec};
-use lazy_static::lazy_static;
-use regex::bytes::Regex;
 use std::ops::Range;
 
-use nom::{
-    branch::{alt, permutation},
-    bytes::complete::*,
-    character::complete::digit1,
-    combinator, multi,
-    sequence::{preceded, separated_pair},
-    IResult,
-};
+use nom::{bytes::complete::*, IResult};
 
 use crate::gfa::*;
 use crate::optfields::*;
 
-// TODO the path in this definitely needs to be redefined, and the parser fixed
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct GAF<T: OptFields> {
     pub seq_name: BString,
     pub seq_len: usize,
     pub seq_range: Range<usize>,
     pub strand: Orientation,
-    pub path: BString,
+    pub path: GAFPath,
     pub path_len: usize,
     pub path_range: Range<usize>,
     pub residue_matches: usize,
@@ -31,7 +21,7 @@ pub struct GAF<T: OptFields> {
     pub optional: T,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum GAFStep {
     SegId(Orientation, BString),
     StableIntv(Orientation, BString, Range<usize>),
@@ -39,18 +29,26 @@ pub enum GAFStep {
 
 impl GAFStep {
     fn parse_orient(bytes: &[u8]) -> IResult<&[u8], Orientation> {
+        use nom::{branch::alt, combinator::map};
         use Orientation::*;
-        let fwd = combinator::map(tag(">"), |_| Forward);
-        let bwd = combinator::map(tag("<"), |_| Backward);
+
+        let fwd = map(tag(">"), |_| Forward);
+        let bwd = map(tag("<"), |_| Backward);
         alt((fwd, bwd))(bytes)
     }
 
     pub fn parse_step(i: &[u8]) -> IResult<&[u8], GAFStep> {
+        use nom::{
+            character::complete::digit1,
+            combinator::{map, opt},
+            sequence::{preceded, separated_pair},
+        };
+
         let (i, orient) = Self::parse_orient(i)?;
         let (i, name) = is_not("<>: \t\r\n")(i)?;
         let name = name.into();
 
-        let parse_digits = combinator::map(digit1, |bs| {
+        let parse_digits = map(digit1, |bs| {
             let s = unsafe { std::str::from_utf8_unchecked(bs) };
             s.parse::<usize>().unwrap()
         });
@@ -60,7 +58,7 @@ impl GAFStep {
             separated_pair(&parse_digits, tag("-"), &parse_digits),
         );
 
-        let (i, range) = combinator::opt(parse_range)(i)?;
+        let (i, range) = opt(parse_range)(i)?;
         if let Some((start, end)) = range {
             let range = start..end;
             Ok((i, GAFStep::StableIntv(orient, name, range)))
@@ -70,6 +68,7 @@ impl GAFStep {
     }
 }
 
+#[derive(Debug, Clone, PartialEq)]
 pub enum GAFPath {
     StableId(BString),
     OrientIntv(Vec<GAFStep>),
@@ -77,23 +76,24 @@ pub enum GAFPath {
 
 impl GAFPath {
     pub fn parse_path(i: &[u8]) -> IResult<&[u8], GAFPath> {
-        let (i, step) = combinator::opt(GAFStep::parse_step)(i)?;
+        use nom::{
+            combinator::{opt, verify},
+            multi::many1,
+        };
+        let (i, path) = opt(many1(GAFStep::parse_step))(i)?;
 
-        if let Some(step) = step {
-            let (i, rest) = multi::many0(GAFStep::parse_step)(i)?;
-            let step: GAFStep = step;
-            let mut rest: Vec<GAFStep> = rest;
-            rest.insert(0, step);
-
-            Ok((i, GAFPath::OrientIntv(rest)))
+        if let Some(path) = path {
+            Ok((i, GAFPath::OrientIntv(path)))
         } else {
-            let (i, stable_id) = is_not("\t")(i)?;
+            let (i, stable_id) = verify(is_not("\t"), |bs: &[u8]| {
+                bs.find_byteset("><").is_none()
+            })(i)?;
             Ok((i, GAFPath::StableId(stable_id.into())))
         }
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct PAF<T: OptFields> {
     pub query_seq_name: BString,
     pub query_seq_len: usize,
@@ -138,11 +138,6 @@ where
     I::Item: AsRef<[u8]>,
     T: OptFields,
 {
-    lazy_static! {
-        static ref RE: Regex =
-            Regex::new(r"(?-u)/([><][^\s><]+(:\d+-\d+)?)+|([^\s><]+)/")
-                .unwrap();
-    }
     let (query_seq_name, query_seq_len, query_seq_range) =
         parse_seq_fields(&mut input)?;
 
@@ -180,18 +175,8 @@ where
     I::Item: AsRef<[u8]>,
     T: OptFields,
 {
-    lazy_static! {
-        static ref RE: Regex =
-            Regex::new(r"(?-u)/([><][^\s><]+(:\d+-\d+)?)+|([^\s><]+)/")
-                .unwrap();
-    }
-
     let paf: PAF<T> = parse_paf(input)?;
-    let path = paf.target_seq_name;
-
-    if !RE.is_match(&path) {
-        return None;
-    }
+    let (_, path) = GAFPath::parse_path(&paf.target_seq_name).ok()?;
 
     Some(GAF {
         path,
@@ -321,6 +306,76 @@ mod tests {
     use super::*;
 
     #[test]
+    fn parse_gaf_lines() {
+        use GAFStep::*;
+        use Orientation::*;
+
+        type GAF = super::GAF<OptionalFields>;
+        let gaf_in1 =
+            b"read1\t6\t0\t6\t+\t>s2>s3>s4\t12\t2\t8\t6\t6\t60\tcg:Z:6M";
+
+        let path_i1: Vec<GAFStep> = ["s2", "s3", "s4"]
+            .iter()
+            .map(|&s| SegId(Forward, s.into()))
+            .collect();
+
+        let expected_1 = GAF {
+            seq_name: "read1".into(),
+            seq_len: 6,
+            seq_range: 0..6,
+            strand: Forward,
+            path: GAFPath::OrientIntv(path_i1),
+            path_len: 12,
+            path_range: 2..8,
+            residue_matches: 6,
+            block_length: 6,
+            quality: 60,
+            optional: vec![OptField::new(b"cg", OptFieldVal::Z("6M".into()))],
+        };
+
+        let gaf_1: Option<GAF> = parse_gaf(gaf_in1.split_str("\t"));
+
+        assert_eq!(Some(expected_1.clone()), gaf_1);
+
+        let gaf_in2 = b"read1\t6\t0\t6\t+\tchr1\t12\t2\t8\t6\t6\t60\tcg:Z:6M";
+
+        let expected_2 = GAF {
+            path: GAFPath::StableId("chr1".into()),
+            ..expected_1
+        };
+
+        let gaf_2: Option<GAF> = parse_gaf(gaf_in2.split_str("\t"));
+
+        assert_eq!(Some(expected_2.clone()), gaf_2);
+
+        let gaf_in3 =
+            b"read2\t7\t0\t7\t-\t>chr1:5-8>foo:8-16\t11\t1\t8\t7\t7\t60\tcg:Z:7M";
+
+        let path_i3: Vec<GAFStep> = vec![
+            StableIntv(Forward, "chr1".into(), 5..8),
+            StableIntv(Forward, "foo".into(), 8..16),
+        ];
+
+        let expected_3 = GAF {
+            seq_name: "read2".into(),
+            seq_len: 7,
+            seq_range: 0..7,
+            strand: Backward,
+            path: GAFPath::OrientIntv(path_i3),
+            path_len: 11,
+            path_range: 1..8,
+            residue_matches: 7,
+            block_length: 7,
+            quality: 60,
+            optional: vec![OptField::new(b"cg", OptFieldVal::Z("7M".into()))],
+        };
+
+        let gaf_3: Option<GAF> = parse_gaf(gaf_in3.split_str("\t"));
+
+        assert_eq!(Some(expected_3), gaf_3);
+    }
+
+    #[test]
     fn parse_gaf_step() {
         use GAFStep::*;
         use Orientation::*;
@@ -365,6 +420,77 @@ mod tests {
         let (i, s) = GAFStep::parse_step(with_tab).unwrap();
         assert_eq!(b"\t266", i);
         assert_eq!(SegId(Backward, "s2".into()), s);
+
+        // Must start with > or <
+
+        // If the ID is followed by a :, that must be followed by an interval
+    }
+
+    #[test]
+    fn parse_gaf_paths() {
+        use GAFPath::*;
+        use GAFStep::*;
+        use Orientation::*;
+
+        use std::ops::Range;
+
+        let seg_fwd = |bs: &str| SegId(Forward, bs.into());
+        let seg_bwd = |bs: &str| SegId(Backward, bs.into());
+        let stbl_fwd =
+            |bs: &str, r: Range<usize>| StableIntv(Forward, bs.into(), r);
+        let stbl_bwd =
+            |bs: &str, r: Range<usize>| StableIntv(Backward, bs.into(), r);
+
+        // stable IDs
+        let p_id1 = b"some_id1";
+        let p_id2 = b"chr1\t123";
+
+        let (i, p) = GAFPath::parse_path(p_id1).unwrap();
+        assert_eq!(b"", i);
+        assert_eq!(StableId("some_id1".into()), p);
+
+        let (i, p) = GAFPath::parse_path(p_id2).unwrap();
+        assert_eq!(b"\t123", i);
+        assert_eq!(StableId("chr1".into()), p);
+
+        println!("{:?}", GAFPath::parse_path(p_id1));
+        println!("{:?}", GAFPath::parse_path(p_id2));
+
+        // oriented paths
+
+        let p_orient1 = b">s1>s2<s3<s4";
+        let p_orient2 = b">chr1:5-8>foo:8-16<bar:16-20\t298";
+        let p_orient3 = b">s1>s2<s3<s4";
+
+        let (i, p) = GAFPath::parse_path(p_orient1).unwrap();
+        assert_eq!(b"", i);
+        assert_eq!(
+            OrientIntv(vec![
+                seg_fwd("s1"),
+                seg_fwd("s2"),
+                seg_bwd("s3"),
+                seg_bwd("s4")
+            ]),
+            p
+        );
+
+        let (i, p) = GAFPath::parse_path(p_orient2).unwrap();
+        assert_eq!(b"\t298", i);
+        assert_eq!(
+            OrientIntv(vec![
+                stbl_fwd("chr1", 5..8),
+                stbl_fwd("foo", 8..16),
+                stbl_bwd("bar", 16..20),
+            ]),
+            p
+        );
+
+        // If the path doesn't start with an orientation, it must be a
+        // stable ID, and thus cannot contain any > or <
+        let err_input = b"s1>s2<s3\t123";
+        let parse_error: IResult<&[u8], GAFPath> =
+            GAFPath::parse_path(err_input);
+        assert!(parse_error.is_err());
     }
 
     #[test]
