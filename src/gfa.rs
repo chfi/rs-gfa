4d9b2df7804708b8a9fb2ff68050db3d8cd17391
Christian <christian@chfi.se>
2020-09-18 11:38:31 -0500

    Unify +/- and >/< Orientation parsers/writers as methods on the type

--- src/gfa.rs
+++ src/gfa.rs
@@ -291,13 +291,58 @@ pub enum Orientation {
     Backward,
 }
 
-// It makes sense for forward to be the default
+impl Orientation {
+    /// Parse an orientation from a single-element, where + is
+    /// Forward, - is Backward
+    pub fn from_bytes_plus_minus<T: AsRef<[u8]>>(bs: T) -> Option<Self> {
+        match bs.as_ref() {
+            b"+" => Some(Orientation::Forward),
+            b"-" => Some(Orientation::Backward),
+            _ => None,
+        }
+    }
+
+    pub fn write_plus_minus(
+        &self,
+        f: &mut std::fmt::Formatter<'_>,
+    ) -> std::fmt::Result {
+        let sym = match self {
+            Self::Forward => '+',
+            Self::Backward => '-',
+        };
+        write!(f, "{}", sym)
+    }
+
+    /// Parse an orientation from a single-element bytestring, where >
+    /// is Forward, < is Backward
+    pub fn from_bytes_gt_ln<T: AsRef<[u8]>>(bs: T) -> Option<Self> {
+        match bs.as_ref() {
+            b">" => Some(Orientation::Forward),
+            b"<" => Some(Orientation::Backward),
+            _ => None,
+        }
+    }
+
+    pub fn write_gt_ln(
+        &self,
+        f: &mut std::fmt::Formatter<'_>,
+    ) -> std::fmt::Result {
+        let sym = match self {
+            Self::Forward => '>',
+            Self::Backward => '<',
+        };
+        write!(f, "{}", sym)
+    }
+}
+
+/// Default orientation is forward
 impl Default for Orientation {
     fn default() -> Orientation {
         Orientation::Forward
     }
 }
 
+/// Forward is true, backward is false
 impl From<Orientation> for bool {
     fn from(o: Orientation) -> bool {
         match o {
@@ -311,35 +356,22 @@ impl Orientation {
     pub fn is_reverse(&self) -> bool {
         !bool::from(*self)
     }
-
-    pub fn from_bytes<T: AsRef<[u8]>>(bs: T) -> Option<Self> {
-        match bs.as_ref() {
-            b"+" => Some(Orientation::Forward),
-            b"-" => Some(Orientation::Backward),
-            _ => None,
-        }
-    }
 }
 
+/// The default parser uses the GFA spec with + as Forward, - as Backward
 impl std::str::FromStr for Orientation {
     type Err = &'static str;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "+" => Ok(Self::Forward),
-            "-" => Ok(Self::Backward),
-            _ => Err("Could not parse orientation (was not + or -)"),
-        }
+        Orientation::from_bytes_plus_minus(s.as_bytes())
+            .ok_or("Could not parse orientation (was not + or -)")
     }
 }
 
+/// Display uses the GFA spec, mapping Forward to "+", Backward to "-"
 impl std::fmt::Display for Orientation {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let sym = match self {
-            Self::Forward => '+',
-            Self::Backward => '-',
-        };
-        write!(f, "{}", sym)
+        self.write_plus_minus(f)
     }
 }
 
