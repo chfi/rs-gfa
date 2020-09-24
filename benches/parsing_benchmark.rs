966ba8a91d7f406ed4f22ca6934b1e6a4c837ad8
Christian <christian@chfi.se>
2020-09-24 18:38:34 -0500

    update bench

--- benches/parsing_benchmark.rs
+++ benches/parsing_benchmark.rs
@@ -62,13 +62,18 @@ macro_rules! bench_gfa {
 
 macro_rules! bench_gfa_noopt {
     ($name:ident, $gfa:literal) => {
-        bench_gfa!(parse_lines_noopt, "excluding_optionals", $name, $gfa);
+        bench_gfa!(parse_lines_usize_noopt, "excluding_optionals", $name, $gfa);
     };
 }
 
 macro_rules! bench_gfa_withopt {
     ($name:ident, $gfa:literal) => {
-        bench_gfa!(parse_lines_withopt, "including_optionals", $name, $gfa);
+        bench_gfa!(
+            parse_lines_usize_withopt,
+            "including_optionals",
+            $name,
+            $gfa
+        );
     };
 }
 
@@ -87,13 +92,13 @@ bench_gfa_withopt!(drb1_sort_withopt, "DRB1-3123.sort.gfa");
 criterion_group!(
     name = no_opt_benches;
     config = Criterion::default().sample_size(25);
-    targets = cov_noopt, a3105_noopt, a3105_sort_noopt, drb1_noopt, drb1_sort_noopt
+    targets = /*cov_noopt,*/ a3105_noopt, a3105_sort_noopt, drb1_noopt, drb1_sort_noopt
 );
 
 criterion_group!(
     name = with_opt_benches;
     config = Criterion::default().sample_size(25);
-    targets = cov_withopt, a3105_withopt, a3105_sort_withopt, drb1_withopt, drb1_sort_withopt
+    targets = /*cov_withopt,*/ a3105_withopt, a3105_sort_withopt, drb1_withopt, drb1_sort_withopt
 );
 
 criterion_main!(no_opt_benches, with_opt_benches);
