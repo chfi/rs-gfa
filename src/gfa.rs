d33a1d4e7efe0ea1f0bc18ebaecc9b5a1997ddd0
Nathan Roach <natproach@gmail.com>
2023-01-22 13:52:06 -0500

    impl-ing display on GFA segments (for SegmentId type in generic field)

--- src/gfa.rs
+++ src/gfa.rs
@@ -6,10 +6,22 @@ pub use self::orientation::*;
 pub use self::traits::*;
 
 use crate::{cigar::CIGAR, optfields::*};
+use std::fmt::{self, Display, Formatter, Write};
 
 use bstr::{BStr, ByteSlice};
 #[cfg(feature = "serde1")]
 use serde::{Deserialize, Serialize};
+
+fn write_optional_fields<U: OptFields, T: Write>(opts: &U, stream: &mut T) {
+    for field in opts.fields() {
+        write!(stream, "\t{}", field).unwrap_or_else(|err| {
+            panic!(
+                "Error writing optional field '{:?}' to stream, {:?}",
+                field, err
+            )
+        })
+    }
+}
 
 /// This module defines the various GFA line types, the GFA object,
 /// and some utility functions and types.
@@ -23,6 +35,45 @@ pub struct GFA<N, T: OptFields> {
     pub links: Vec<Link<N, T>>,
     pub containments: Vec<Containment<N, T>>,
     pub paths: Vec<Path<N, T>>,
+}
+
+impl<N: SegmentId, T: OptFields> Display for GFA<N, T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}\n", self.header,)
+            .expect("Error writing header to GFA stream.");
+
+        for segment in self.segments.iter() {
+            write!(f, "{}\n", segment).expect(&format!(
+                "Error writing segment {} to GFA stream",
+                segment.name.display()
+            ));
+        }
+
+        for link in self.links.iter() {
+            write!(f, "{}\n", link).expect(&format!(
+                "Error writing link from {} to {} to GFA stream",
+                link.from_segment.display(),
+                link.to_segment.display()
+            ));
+        }
+
+        for containment in self.containments.iter() {
+            write!(f, "{}\n", containment).expect(&format!(
+                "Error writing containment {} within {} to GFA stream",
+                containment.contained_name.display(),
+                containment.container_name.display()
+            ));
+        }
+
+        for path in self.paths.iter() {
+            write!(f, "{}\n", path).expect(&format!(
+                "Error writing path {} to GFA stream",
+                path.path_name.display()
+            ));
+        }
+
+        Ok(())
+    }
 }
 
 /// Enum containing the different kinds of GFA lines.
@@ -148,6 +199,18 @@ impl<T: OptFields> Default for Header<T> {
     }
 }
 
+impl<T: OptFields> Display for Header<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "H",)?;
+        if let Some(v) = &self.version {
+            write!(f, "\tVN:Z:{}", v.as_bstr())
+                .expect("Error writing version number in header line.")
+        }
+        write_optional_fields(&self.optional, f);
+        Ok(())
+    }
+}
+
 /// A segment in a GFA graph. Generic over the name type, but
 /// currently the parser is only defined for N = Vec<u8>
 #[derive(Default, Debug, Clone, PartialEq, PartialOrd, Hash)]
@@ -166,6 +229,15 @@ impl<T: OptFields> Segment<Vec<u8>, T> {
             sequence: Vec::from(sequence),
             optional: Default::default(),
         }
+    }
+}
+
+impl<N: SegmentId, U: OptFields> Display for Segment<N, U> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "S\t{}\t{}", self.name.display(), self.sequence.as_bstr())?;
+        write_optional_fields(&self.optional, f);
+
+        Ok(())
     }
 }
 
@@ -200,6 +272,23 @@ impl<T: OptFields> Link<Vec<u8>, T> {
     }
 }
 
+impl<N: SegmentId, U: OptFields> Display for Link<N, U> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "L\t{}\t{}\t{}\t{}\t{}",
+            self.from_segment.display(),
+            self.from_orient,
+            self.to_segment.display(),
+            self.to_orient,
+            self.overlap.as_bstr(),
+        )
+        .expect("Error writing link to stream");
+        write_optional_fields(&self.optional, f);
+        Ok(())
+    }
+}
+
 #[derive(Default, Debug, Clone, PartialEq, PartialOrd, Hash)]
 #[cfg_attr(feature = "serde1", derive(Serialize, Deserialize))]
 pub struct Containment<N, T: OptFields> {
@@ -210,6 +299,24 @@ pub struct Containment<N, T: OptFields> {
     pub pos: usize,
     pub overlap: Vec<u8>,
     pub optional: T,
+}
+
+impl<N: SegmentId, T: OptFields> Display for Containment<N, T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "C\t{}\t{}\t{}\t{}\t{}\t{}",
+            self.container_name.display(),
+            self.container_orient,
+            self.contained_name.display(),
+            self.contained_orient,
+            self.pos,
+            self.overlap.as_bstr()
+        )
+        .expect("Error writing containment to stream");
+        write_optional_fields(&self.optional, f);
+        Ok(())
+    }
 }
 
 /// The step list that the path actually consists of is an unparsed
@@ -240,6 +347,29 @@ impl<N: SegmentId, T: OptFields> Path<N, T> {
             optional,
             _segment_names: std::marker::PhantomData,
         }
+    }
+}
+
+impl<N, T: OptFields> Display for Path<N, T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "P\t{}\t", self.path_name.as_bstr())
+            .expect("Error writing path to stream");
+
+        write!(f, "{}\t", self.segment_names.as_bstr()).unwrap();
+
+        self.overlaps.iter().enumerate().for_each(|(i, o)| {
+            if i != 0 {
+                write!(f, ",").unwrap();
+            }
+            match o {
+                None => write!(f, "*").unwrap(),
+                // Some(o) => write!(f, "{}", o.to_str().unwrap()).unwrap(),
+                Some(o) => write!(f, "{}", o).unwrap(),
+            }
+        });
+
+        write_optional_fields(&self.optional, f);
+        Ok(())
     }
 }
 
@@ -335,6 +465,9 @@ impl<N, T: OptFields> Containment<N, T> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::io::Read as IoRead;
+    use std::io::Write as IoWrite;
+    use tempfile::NamedTempFile;
 
     #[test]
     fn path_iter() {
@@ -366,5 +499,190 @@ mod tests {
             let seg = line.some_segment();
             println!("{:?}", seg);
         }
+    }
+
+    #[test]
+    fn write_segment_to_string_buffer() {
+        use OptFieldVal::*;
+        let mut segment: Segment<Vec<u8>, OptionalFields> =
+            Segment::new(b"seg1", b"GCCCTA");
+        let opt_ij = OptField::new(b"IJ", A(b'x'));
+        let opt_ab = OptField::new(b"AB", BInt(vec![1, 2, 3, 52124]));
+        let opt_ur =
+            OptField::new(b"UR", Z(Vec::<u8>::from("http://test.com/")));
+        let opt_rc = OptField::new(b"RC", Int(123));
+        segment.optional = vec![opt_rc, opt_ur, opt_ij, opt_ab];
+        let expected = "S\tseg1\tGCCCTA\tRC:i:123\tUR:Z:http://test.com/\tIJ:A:x\tAB:B:I1,2,3,52124";
+        let mut string = String::new();
+        write!(&mut string, "{}", segment)
+            .expect("Error writing to string buffer");
+        assert_eq!(string, expected);
+    }
+
+    #[test]
+    fn write_link_to_string_buffer() {
+        let link: Link<Vec<u8>, ()> = Link::new(
+            b"13",
+            Orientation::Forward,
+            b"552",
+            Orientation::Backward,
+            b"0M",
+        );
+        let mut string = String::new();
+        write!(&mut string, "{}", link)
+            .expect("Error writing to string buffer");
+        assert_eq!(string, "L\t13\t+\t552\t-\t0M");
+    }
+
+    #[test]
+    fn write_path_to_string_buffer() {
+        use crate::cigar::CIGAR;
+
+        let cigars = vec![b"8M", b"1M", b"3M"]
+            .iter()
+            .map(|bs| CIGAR::from_bytestring(&bs[..]))
+            .collect();
+
+        let path: Path<Vec<u8>, _> =
+            Path::new("path1".into(), "13+,51-,241+".into(), cigars, ());
+
+        let mut string = String::new();
+        write!(&mut string, "{}", path)
+            .expect("Error writing to string buffer");
+        assert_eq!(string, "P\tpath1\t13+,51-,241+\t8M,1M,3M");
+    }
+
+    #[test]
+    fn write_gfa_to_string_buffer() {
+        use std::io::Read;
+        use std::path::PathBuf;
+
+        let parser = crate::parser::GFAParser::new();
+        let in_gfa: GFA<Vec<u8>, ()> =
+            parser.parse_file(&"./test/gfas/lil.gfa").unwrap();
+
+        let mut file =
+            std::fs::File::open(&PathBuf::from("./test/gfas/lil.gfa")).unwrap();
+        let mut file_string = String::new();
+        file.read_to_string(&mut file_string).unwrap();
+
+        let mut string = String::new();
+        write!(&mut string, "{}", in_gfa)
+            .expect("Error writing to string buffer");
+
+        assert_eq!(string, file_string);
+    }
+
+    #[test]
+    fn write_segment_to_file_buffer() {
+        use OptFieldVal::*;
+        let mut segment: Segment<Vec<u8>, OptionalFields> =
+            Segment::new(b"seg1", b"GCCCTA");
+        let opt_ij = OptField::new(b"IJ", A(b'x'));
+        let opt_ab = OptField::new(b"AB", BInt(vec![1, 2, 3, 52124]));
+        let opt_ur =
+            OptField::new(b"UR", Z(Vec::<u8>::from("http://test.com/")));
+        let opt_rc = OptField::new(b"RC", Int(123));
+        segment.optional = vec![opt_rc, opt_ur, opt_ij, opt_ab];
+        let expected = "S\tseg1\tGCCCTA\tRC:i:123\tUR:Z:http://test.com/\tIJ:A:x\tAB:B:I1,2,3,52124";
+
+        let mut tempfile =
+            NamedTempFile::new().expect("Error creating temp file");
+        tempfile
+            .write_all(format!("{}", segment).as_bytes())
+            .expect("Error writing to file buffer.");
+
+        let mut tempfile_reader =
+            tempfile.reopen().expect("error re-opening temp file.");
+
+        let mut string = String::new();
+        tempfile_reader
+            .read_to_string(&mut string)
+            .expect("Error parsing file");
+        assert_eq!(string, expected);
+    }
+
+    #[test]
+    fn write_link_to_file_buffer() {
+        let link: Link<Vec<u8>, ()> = Link::new(
+            b"13",
+            Orientation::Forward,
+            b"552",
+            Orientation::Backward,
+            b"0M",
+        );
+        let mut tempfile =
+            NamedTempFile::new().expect("Error creating temp file");
+        tempfile
+            .write_all(format!("{}", link).as_bytes())
+            .expect("Error writing to file buffer");
+
+        let mut tempfile_reader =
+            tempfile.reopen().expect("error re-opening temp file.");
+
+        let mut string = String::new();
+        tempfile_reader
+            .read_to_string(&mut string)
+            .expect("Error parsing file");
+        assert_eq!(string, "L\t13\t+\t552\t-\t0M");
+    }
+
+    #[test]
+    fn write_path_to_file_buffer() {
+        use crate::cigar::CIGAR;
+
+        let cigars = vec![b"8M", b"1M", b"3M"]
+            .iter()
+            .map(|bs| CIGAR::from_bytestring(&bs[..]))
+            .collect();
+
+        let path: Path<Vec<u8>, _> =
+            Path::new("path1".into(), "13+,51-,241+".into(), cigars, ());
+
+        let mut tempfile =
+            NamedTempFile::new().expect("Error creating temp file");
+
+        tempfile
+            .write_all(format!("{}", path).as_bytes())
+            .expect("Error writing to file buffer");
+
+        let mut tempfile_reader =
+            tempfile.reopen().expect("error re-opening temp file.");
+
+        let mut string = String::new();
+        tempfile_reader
+            .read_to_string(&mut string)
+            .expect("Error parsing file");
+
+        assert_eq!(string, "P\tpath1\t13+,51-,241+\t8M,1M,3M");
+    }
+
+    #[test]
+    fn write_gfa_to_file_buffer() {
+        use std::path::PathBuf;
+
+        let parser = crate::parser::GFAParser::new();
+        let in_gfa: GFA<Vec<u8>, ()> =
+            parser.parse_file(&"./test/gfas/lil.gfa").unwrap();
+
+        let mut file =
+            std::fs::File::open(&PathBuf::from("./test/gfas/lil.gfa")).unwrap();
+        let mut file_string = String::new();
+        file.read_to_string(&mut file_string).unwrap();
+
+        let mut tempfile =
+            NamedTempFile::new().expect("Error creating temp file");
+        write!(&mut tempfile, "{}", in_gfa)
+            .expect("Error writing to file buffer");
+
+        let mut tempfile_reader =
+            tempfile.reopen().expect("error re-opening temp file.");
+
+        let mut string = String::new();
+        tempfile_reader
+            .read_to_string(&mut string)
+            .expect("Error parsing file");
+
+        assert_eq!(string, file_string);
     }
 }
